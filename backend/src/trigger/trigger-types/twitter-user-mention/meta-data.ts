de4c09f2e5f0592e869201ade18d59f0c3c03fb0
Darragh ORiordan <darragh.oriordan@gmail.com>
2021-10-20 07:39:36 +1100

    feat: add props to twitter user meta

--- backend/src/trigger/trigger-types/twitter-user-mention/meta-data.ts
+++ backend/src/trigger/trigger-types/twitter-user-mention/meta-data.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class TwitterUserMentionMeta {
+    @ApiProperty()
     public twitterUserName!: string;
+    @ApiProperty()
     public mentionText!: string;
+    @ApiProperty()
     public inLastSeconds!: number;
 }
